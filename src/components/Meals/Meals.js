1c6472858834a8f3e93ed16642e412d32e2c3efc
SuraZz007 <surajdev9886@gmail.com>
2022-09-05 14:08:15 +0530

    make meals summary and add demo meals list

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import AvailableMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+export default Meals;
